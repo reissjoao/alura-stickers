cb061fc8809e2bb04b71c9da9a7c4f88101f87e9
reissjoao <joaooreis@hotmail.com>
2023-03-28 21:57:30 -0300

    add stickers generator

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -24,12 +26,21 @@ public class App {
 
         // Exibir e manipular os dados 
         for (Map<String,String> filme : listaDeFilmes) {
+
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("title");
+            
+            InputStream inputStream = new URL(urlImagem).openStream();
+            String nomeArquivo = titulo + ".png";
+
+            var geradora = new GeradoraDeFigurinhas();
+            geradora.cria(inputStream, nomeArquivo);
+
             System.out.println("\u001b[1mTítulo:\u001b[m " + filme.get("title"));
             System.out.println("URL da imagem: " + filme.get("image"));
             System.out.println("\u001b[1mClassificação:\u001b[m " + filme.get("imDbRating"));
             System.out.println("\u001b[1mRank:\u001b[m " + filme.get("rank"));
-            System.out.println();
-            
-            }
+            System.out.println();            
+        }
     }
 }
